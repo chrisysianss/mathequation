b37144bffbd325ecb707c0b2222169edcd86d9c0
chrisysianss <116235477+chrisysianss@users.noreply.github.com>
2022-10-19 17:14:13 -1000

    Add files via upload

---
+++ MathEquation/src/App.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+    // int a = 4;
+    // int b = 9;
+    // int c = 5;
+    // int d = 6;
+    // mulit(a,b,c,d);
+    // }
+    division math = new division(4, 9, 5, 6);
+    System.out.println(math.math());
+
+    // static public void mulit(int a,int b,int c,int d) {
+    // System.out.println(a+b/c*d);
+
+    // }
+
+    }
+
+}
+//a=1.5
+//b=6
+//c=5
+//d=10
+//compare a * b to c * d
+//add a + b / 2 * 5
---
+++ MathEquation/src/division.java
@@ -0,0 +1,19 @@
+public class division {
+    int a;
+    int b;
+    int c;
+    int d;
+    public division(int a, int b, int c, int d)
+    {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+
+    }
+    public int math(){
+
+        return a+b/c*d;
+    }
+
+}
